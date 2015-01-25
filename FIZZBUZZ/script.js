e57e153acd2f13927a99a3b9f2d4053f11b799ce
MateoEnterprises <mat.thompson02@gmail.com>
2015-01-25 16:27:52 -0700

    created FizzBuzz 'app'

---
+++ FIZZBUZZ/script.js
@@ -0,0 +1,14 @@
+var fizzbuzz = [];
+var fizFun = function () {
+  for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0 ) {
+      if (i % 5 === 0) {
+        fizzbuzz.push("fizzbuzz");
+      } else { fizzbuzz.push("fizz"); }
+    } else if(i % 5 === 0) {
+      fizzbuzz.push("buzz");
+    } else { fizzbuzz.push(i);}
+  }
+  alert(fizzbuzz);
+}
+      
